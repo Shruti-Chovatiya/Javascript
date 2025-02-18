b17dedbc8c2580d6093efbae69cd1502e181102f
Shruti-Chovatiya <152643069+Shruti-Chovatiya@users.noreply.github.com>
2025-02-18 14:21:32 +0000

    variables done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 12;
+let accountEMail = "shruti@gmail.com";
+var accountPassword = "1230";
+accountCity = "Pune";
+
+console.log(accountEMail);
+
+console.table([accountId, accountEMail, accountPassword, accountCity]);
