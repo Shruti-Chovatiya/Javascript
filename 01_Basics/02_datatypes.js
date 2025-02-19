ef874e56b9a56a0d4d72f5937465d418ccefdac1
Shruti-Chovatiya <152643069+Shruti-Chovatiya@users.noreply.github.com>
2025-02-19 12:59:34 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; //treat all js code as newer version
+
+let userName = "Shruti"; //string
+let num = 20; //number
+let age; //undefined
+//let isLoggedin = flase = boolean
+// let word = none = null
+//bigint for larger number ex-stockmareket values
+//symbol = unique
+
+
+//object
+
+console.log(typeof userName);
+console.log(typeof age);
