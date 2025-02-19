a3cecf3268d5dbff9cd434cae36ce088868cfa72
Shruti-Chovatiya <152643069+Shruti-Chovatiya@users.noreply.github.com>
2025-02-19 13:39:13 +0000

    conersion of datatypes

---
+++ 01_Basics/03_conversion-operation.js
@@ -0,0 +1,24 @@
+let score = "35asd";
+
+//console.log(typeof score);
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"35" -> 35
+//"35abc" -> Nan
+//"true" = 1, "false" = 0
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+// 1 -> true, 0->false
+// "" -> false, "abc"-> true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
