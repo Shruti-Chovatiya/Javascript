9126c91d49ae1686d083d587097c646ef9f20954
Shruti-Chovatiya <152643069+Shruti-Chovatiya@users.noreply.github.com>
2025-02-19 14:19:44 +0000

    done

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 == 1);
+// console.log(2 <= 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 1);
+// console.log(null >= 1);
+// console.log(null == 1);
+
+// console.log(undefined == 1);
+// console.log(undefined > 1);
+// console.log(undefined < 1);
+// console.log(undefined >= 1);
+
+// ===
+console.log("2" === 2); //false
+console.log(1 === 1); //true
