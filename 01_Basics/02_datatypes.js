496ecd904e7058f0a2ae5f68ef3476d075a7ded5
Shruti-Chovatiya <152643069+Shruti-Chovatiya@users.noreply.github.com>
2025-02-19 16:32:12 +0000

    datatypes

--- 01_Basics/02_datatypes.js
+++ 01_Basics/02_datatypes.js
@@ -19,7 +19,21 @@ console.log(typeof age);
 //A. primitive
 //7 types = string, number, boolean, null, undefined, BigInt, symbol.
 
-
-
 //B. reference type (non-primtive)
 // Array, Objects, Functions.
+
+// Return type of variables in JavaScript
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
+
