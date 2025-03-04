922151f072b37cfaef4105c0720d63b7838c00c2
Shruti-Chovatiya <152643069+Shruti-Chovatiya@users.noreply.github.com>
2025-03-04 13:37:12 +0000

    obj

---
+++ 02_Basics/04_objects.js
@@ -0,0 +1,36 @@
+//singleton object 
+// const socialApp = new Object()
+
+// non singleton object
+const socialApp = {}
+
+socialApp.id = "123as";
+socialApp.name = "shruti";
+socialApp.isLoggedin = false;
+
+console.log(socialApp);
+
+const user = {
+    email : "shruti@gmail.com",
+    fullname : {
+        userFullname :{
+            firstname : "shruti",
+            lastname : "chovatiya"
+        }
+    }
+}
+
+console.log(user.fullname.userFullname.firstname);
+
+const obj1 = {1 : "a", 2 : "b"}
+const obj2 = {3 : "c", 4 : "d"}
+
+// const obj3 = Object.assign({}, obj1, obj2)
+
+const obj3= {...obj1, ...obj2}
+console.log(obj3);
+
+console.log(Object.keys(socialApp));
+console.log(Object.values(socialApp));
+console.log(Object.entries(socialApp));
+console.log(Object.hasOwnProperty("name"));
