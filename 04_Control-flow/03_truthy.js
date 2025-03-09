c8bde7397cd1240e969d8e966a4e8ba3b2d3436a
Shruti-Chovatiya <152643069+Shruti-Chovatiya@users.noreply.github.com>
2025-03-09 05:49:23 +0000

    commit

--- 04_Control-flow/03_truthy.js
+++ 04_Control-flow/03_truthy.js
@@ -15,6 +15,28 @@ if (userEmail) {
 
 //"0", "false", " ", [], {}, function(){}
 
-if (userEmail.length === 0) {
-    console.log("Empty array")
+//check empty array
+// if (userEmail.length === 0) {
+//     console.log("Empty array")
+// }
+
+//check empty object
+const emptyObject = {}
+if (Object.keys(emptyObject).length === 0) {
+    console.log("object is empty");
+    
 }
+
+//nulish coalescing operator (??): null undefined
+
+// let val1 = 5 ?? 10;
+// let val1 = null ?? 10;
+// let val1 = undefined ?? 10;
+
+// console.log(val1);
+
+//terniary operator
+///condition ? true : false
+
+const iceTeaPrice = 50;
+iceTeaPrice > 40 ? console.log("true") : console.log("False");
