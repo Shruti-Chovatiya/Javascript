34e7ae0f02bb33e66f52712a34f38d69a395db6d
Shruti-Chovatiya <152643069+Shruti-Chovatiya@users.noreply.github.com>
2025-03-15 09:58:46 +0000

    commit

---
+++ 05_Iterations/05_map-reduce.js
@@ -0,0 +1,18 @@
+// const code = ["js", "python", "c", "cpp"];
+
+// code.forEach((item) => {
+//     console.log(item);
+    
+// });
+
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const newNum = myNums.filter( (num) => num > 4 );
+
+console.log(newNum);
+
+const addNum = myNums.map( (nums) =>   nums + 10 );
+console.log(addNum);
+
+
